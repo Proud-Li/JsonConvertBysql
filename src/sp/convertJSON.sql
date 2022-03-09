7198fbaa602cd06dfdff660be265bd923d21f89a
Proud-Li <zhiholi@sina.com>
2022-03-09 15:07:38 +0800

    行列转换加类型处理

--- src/sp/convertJSON.sql
+++ src/sp/convertJSON.sql
@@ -9,8 +9,9 @@ GO
 ******************************************************************************************
 Author:     PD
 Date:       20220304
-
-Note:
+Note:       JSON->Hierarchy->Table 包含 行列转换
+1效率不高 不要用在生产环境
+2数字字段 不要有null 不然只能处理成字符
 
 Modified history
 -----------------
@@ -24,7 +25,6 @@ desc
 dbcc dropcleanbuffers
 --从过程缓存中删除所有元素。
 dbcc freeproccache
-
 
 */
 
@@ -48,40 +48,65 @@ BEGIN
     
 	BEGIN TRY
 
-	declare @tmp1 Hierarchy
+	    declare @tmp1 Hierarchy
+
+        select * into #tmp2
+        from @tmp1 a
+
+	    insert into @tmp1
+		    (element_id,sequenceNo,parent_ID,[Object_ID],[NAME],StringValue,ValueType)
+	    select a.element_id,a.sequenceNo,a.parent_ID,a.[Object_ID],a.[NAME],a.StringValue,a.ValueType
+	    from dbo.parseJSON(@json) a
 
 	 
-	declare @tmp2 Hierarchy
 
-	insert into @tmp1
-		(element_id,sequenceNo,parent_ID,[Object_ID],[NAME],StringValue,ValueType)
-	select a.element_id,a.sequenceNo,a.parent_ID,a.[Object_ID],a.[NAME],a.StringValue,a.ValueType
-	from dbo.parseJSON(@json) a
+	    insert into #tmp2
+		    (element_id,sequenceNo,parent_ID,[Object_ID],[NAME],StringValue,ValueType)
+	    select a.element_id,a.sequenceNo,a.parent_ID,a.[Object_ID],a.[NAME],a.StringValue,a.ValueType
+	    from @tmp1 a
+	    left join @tmp1 b on a.parent_ID=b.[Object_ID]
+	    left join @tmp1 c on b.parent_ID=c.[Object_ID]
+	    where c.[NAME]=@NAME
+ 
 
-	 
+        select *
+        into #tmp_name
+        from (
+            select a.NAME,ValueType=max(a.ValueType),element_id=max(a.element_id)
+            from #tmp2 a 
+            group by a.NAME
+        )a
+        order by a.element_id
 
-	insert into @tmp2
-		(element_id,sequenceNo,parent_ID,[Object_ID],[NAME],StringValue,ValueType)
-	select a.element_id,a.sequenceNo,a.parent_ID,a.[Object_ID],a.[NAME],a.StringValue,a.ValueType
-	from @tmp1 a
-	left join @tmp1 b on a.parent_ID=b.[Object_ID]
-	left join @tmp1 c on b.parent_ID=c.[Object_ID]
-	where c.[NAME]=@NAME
 
+        --if @Debug=1 select * from #tmp_name
 
-	select @sql="
-		select a.parent_ID"
-	select @sql=@sql+"  
-			,max(case when a.[NAME]='"+a.[NAME]+"' then a.StringValue else '' end) as ["+a.[NAME]+"]"  
-	from (select distinct [NAME] from @tmp2 a) a  
+	    select @sql="
+		    select a.parent_ID"
+	    select @sql=@sql+"
+			    ,"
+                +"max(case when a.[NAME]='"+a.[NAME]+"' then "
+                + case a.ValueType
+                    when 'string' then "nullif(a.StringValue,'null')"
+                    when 'int' then "convert("+a.ValueType+",a.StringValue)"
+                    when 'bigint' then "convert("+a.ValueType+",a.StringValue)"
+                    when 'dec(18,6)' then "convert("+a.ValueType+",a.StringValue)"
+                    when 'dec(26,12)' then "convert("+a.ValueType+",a.StringValue)"
+                    else "nullif(a.StringValue,'null')" end
+                    --else "a.StringValue" end
+                +" else null end)" 
+                +" as ["+a.[NAME]+"]" 
+	    from #tmp_name a  
 
-	set @sql = @sql +"
-		from #tmp2 a   
-		group by a.parent_ID 
-		"
-	--print(@sql)  
-	select convert(xml,@sql)
-	exec(@sql)  
+	    set @sql = @sql +"
+		    from #tmp2 a   
+		    group by a.parent_ID 
+		    "
+
+	    print(@sql)  
+
+	    --select sqlxml=convert(xml,@sql)
+	    exec(@sql)
 
 
 	END TRY
